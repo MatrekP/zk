83c7101d14448137f92f83f4fff7924d0c6bd1ca
MatrekP <44618711+MatrekP@users.noreply.github.com>
2019-02-10 22:32:19 +0100

    Update tema21.py

--- tema21.py
+++ tema21.py
@@ -7,47 +7,7 @@ a/b = p/q operator r/s
 """
 
 
-def soucet(p, q, r, s):
-    a = p*s + q*r
-    b = q*s
-    delitel = kraceni(a, b)
-    a = a / delitel
-    b = b / delitel
-    return a, b
-
-
-def nasobeni(p, q, r, s):
-    a = p*r
-    b = q*s
-    delitel = kraceni(a, b)
-    a = a / delitel
-    b = b / delitel
-    return a, b
-
-
-def deleni(p, q, r, s):
-    a = p*s
-    b = q*r
-    delitel = kraceni(a, b)
-    a = a / delitel
-    b = b / delitel
-    return a, b
-
-
-def kraceni(a, b):
-    if b == 0:
-        return a
-    else:
-        return kraceni(b, a % b)
-
-
-p = 8.5
-q = 2
-r = 6
-s = 5
-
-
-if (type(p) == int or float) and (type(q) == int or float) and (type(r) == int or float) and (type(s) == int or float):
+def operace(p, q, r, s):
     if q == 0 or s == 0:
         print("Deleni nulou!")
     else:
@@ -57,5 +17,63 @@ if (type(p) == int or float) and (type(q) == int or float) and (type(r) == int o
             print("Deleni nulou!")
         else:
             print(deleni(p, q, r, s))
-else:
-    print("Spatny vstup!")
+
+
+def soucet(p, q, r, s):
+    a = p*s + q*r
+    b = q*s
+    delitel = kraceni(a, b)
+    a = a / delitel
+    b = b / delitel
+    if b == 1:
+        return a
+    else:
+        return a, b
+
+
+def nasobeni(p, q, r, s):
+    a = p*r
+    b = q*s
+    delitel = kraceni(a, b)
+    a = a / delitel
+    b = b / delitel
+    if b == 1:
+        return a
+    else:
+        return a, b
+
+
+def deleni(p, q, r, s):
+    a = p*s
+    b = q*r
+    delitel = kraceni(a, b)
+    a = a / delitel
+    b = b / delitel
+    if b == 1:
+        return a
+    else:
+        return a, b
+
+
+def kraceni(a, b):
+    # if b == 0:
+    #     return a
+    # else:
+    #     return kraceni(b, a % b)
+    return 1
+
+
+# try:
+#     p = float(input("p: "))
+#     q = float(input("q: "))
+#     r = float(input("r: "))
+#     s = float(input("s: "))
+# except ValueError:
+#     print("Spatny vstup!")
+#     exit(1)
+
+p = 2
+q = 3.2
+r = 5
+s = 2
+operace(p, q, r, s)
