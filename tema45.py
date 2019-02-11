7421f039a877cfe82606c47de711e2c30b92af5d
MatrekP <44618711+MatrekP@users.noreply.github.com>
2019-02-11 13:52:44 +0100

    Update tema45.py

--- tema45.py
+++ tema45.py
@@ -6,9 +6,10 @@ Nalezeni k-teho nejvetsiho prvku v posloupnosti
 from random import randrange
 
 
+# zadani vstupu se zkouskou spravnosti vstupu
 try:
     n = int(input("pocet prvku: "))
-    k = int(input("k-ty prvek: "))
+    k = int(input("k-ty nejvetsi prvek: "))
     min = int(input("minimum: "))
     max = int(input("maximum: "))
 except ValueError:
@@ -16,13 +17,15 @@ except ValueError:
     exit(1)
 
 
-if n > 0 and k > 0 and k <= n:
+# zkontrolovani spravnosti oboru hodnot vstupu s vytvorenim posloupnosti a nalezením k-teho nejvetsiho prvku
+if n > 0 and k > 0 and k <= n and min > 0 and max > 0:
     seznam = []
     for i in range(0,n):
-        seznam.append(randrange(min,max))
+        seznam.append(randrange(min, max))
     print(seznam)
     seznam.sort()
     print(seznam)
     print(seznam[-k])
+    print("Odpocivej v pokoji.")
 else:
     print("Chybny vstup!")
